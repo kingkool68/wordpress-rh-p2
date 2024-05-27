3e04bbc182deecbd9f1e0c0d54be2c4a3d9a9d8e
Russell Heimlich <kingkool68@gmail.com>
2024-05-26 22:37:23 -0400

    Style post meta stuff

--- single-post.php
+++ single-post.php
@@ -1,20 +1,20 @@
 <?php
 setup_postdata( get_post() );
 
-$date      = RH_Helpers::get_date_values( $post->post_date );
+$date      = get_post_datetime( $post );
 $author_id = get_the_author_meta( 'ID' );
 
 $context = array(
-	'the_title'             => get_the_title(),
-	'the_author'            => get_the_author_meta( 'display_name', $author_id ),
-	'the_author_avatar_url' => get_avatar_url(
+	'the_title'                => get_the_title(),
+	'the_author'               => get_the_author_meta( 'display_name', $author_id ),
+	'the_author_avatar_url'    => get_avatar_url(
 		$author_id,
 		array(
-			'size' => 96,
+			'size' => 80,
 		)
 	),
-	'display_date'          => $date->display_date,
-	'machine_date'          => $date->machine_date,
-	'the_content'           => apply_filters( 'the_content', get_the_content() ),
+	'the_publish_date'         => $date->format( 'g:i a \o\n F j, Y' ),
+	'the_publish_machine_date' => $date->format( DATE_W3C ),
+	'the_content'              => apply_filters( 'the_content', get_the_content() ),
 );
 Sprig::out( 'single-post.twig', $context );
