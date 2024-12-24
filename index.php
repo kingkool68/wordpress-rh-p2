94ffecc190bac3ac4b4d16b8b50d9e0a5d1f813f
Russell Heimlich <kingkool68@gmail.com>
2024-12-23 20:33:37 -0500

    Add a page title for tag archives

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
+$the_title          = '';
+$the_queried_object = get_queried_object();
+if ( ! empty( $the_queried_object->name ) ) {
+	$the_title = apply_filters( 'the_title', $the_queried_object->name );
+	$the_title = 'Tagged #' . $the_title;
+}
+
 $context = array(
+	'the_title'  => $the_title,
 	'the_posts'  => RH_Posts::render_from_wp_query(),
 	'pagination' => RH_Pagination::render_from_wp_query(),
 );
