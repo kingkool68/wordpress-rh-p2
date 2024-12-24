04baa9e61bafa95e083251ffe6877d88f40c9bbc
kingkool68 <kingkool68+github-actions[bot]@users.noreply.github.com>
2024-12-24 00:49:07 +0000

    Merged 3d675eef42fc65c24cd612c1b5c12ff44711d4da from main
    https://github.com/kingkool68/wordpress-rh-p2/commit/3d675eef42fc65c24cd612c1b5c12ff44711d4da/checks

--- header.php
+++ header.php
@@ -1,8 +1,16 @@
 <?php
+$main_nav_args = array(
+	'theme_location' => 'main',
+	'container'      => false,
+	'items_wrap'     => '%3$s', // Prevents wrapping in any markup
+	'echo'           => false,
+);
+	$main_nav  = wp_nav_menu( $main_nav_args );
+
+
 $context = array(
-	'site_url'   => get_site_url(),
-	'site_name'  => get_bloginfo( 'name' ),
-	'close_icon' => RH_SVG::get_icon( 'close' ),
-	'menu_icon'  => RH_SVG::get_icon( 'menu' ),
+	'site_url'  => get_site_url(),
+	'site_name' => get_bloginfo( 'name' ),
+	'main_nav'  => $main_nav,
 );
 Sprig::out( 'header.twig', $context );
